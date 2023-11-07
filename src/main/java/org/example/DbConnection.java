f240e920e27db0f48d9fcfa37f1037ab8474cd71
Robertson Souza <robertson.dcs@gmail.com>
2023-11-07 00:21:28 -0300

    improving execution

--- src/main/java/org/example/DbConnection.java
+++ src/main/java/org/example/DbConnection.java
@@ -4,6 +4,7 @@ import org.apache.commons.dbcp2.BasicDataSource;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DbConnection {
@@ -53,15 +54,82 @@ public class DbConnection {
             preparedStatement = conn.prepareStatement("CREATE TABLE person(id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL)");
             preparedStatement.executeUpdate();
             preparedStatement.close();
-            System.out.println("Database dropped and created");
+            System.out.println("DB initialized");
             conn.close();
         } catch (SQLException e) {
             System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    public static void countRows() throws SQLException {
+        Connection conn = connect();
+        try  {
+            PreparedStatement preparedStatement = conn.prepareStatement("SELECT count(*) AS recordCount from person");
+            ResultSet rs = preparedStatement.executeQuery();
+            rs.next();
+            int count = rs.getInt("recordCount");
+            preparedStatement.close();
+            rs.close();
+            System.out.println("DB rows count: " + count);
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }else {
+                System.out.println("Without connection");
+            }
+        }
+    }
 
+    public static void getFirstRecord() throws SQLException {
+        Connection conn = connect();
+        try  {
+            PreparedStatement preparedStatement = conn.prepareStatement("SELECT name FROM person ORDER BY id LIMIT 1");
+            ResultSet rs = preparedStatement.executeQuery();
+            rs.next();
+            String name = rs.getString("name");
+            preparedStatement.close();
+            rs.close();
+            System.out.println("First db row: " + name);
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }else {
+                System.out.println("Without connection");
+            }
+        }
+    }
+
+    public static void getLastRecord() throws SQLException {
+        Connection conn = connect();
+        try  {
+            PreparedStatement preparedStatement = conn.prepareStatement("SELECT name FROM person ORDER BY id DESC LIMIT 1");
+            ResultSet rs = preparedStatement.executeQuery();
+            rs.next();
+            String name = rs.getString("name");
+            preparedStatement.close();
+            rs.close();
+            System.out.println("Last db row: " + name);
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }else {
+                System.out.println("Without connection");
+            }
+        }
     }
 }
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,41 +20,45 @@ import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) throws IOException, SQLException {
-        int sampleSize = 1000000;
 
-        List<String> listOfPeople = new ArrayList<>();
+        for (int sampleSize = 1000; sampleSize <= 1000000; sampleSize *= 10) {
+            System.out.println("\n*--------- New execution: sample size " + sampleSize + " ----------*");
+            List<String> listOfPeople = new ArrayList<>();
+            try {
+                XSSFWorkbook workbook = (XSSFWorkbook) WorkbookFactory.create(new FileInputStream("sample/data"+ sampleSize +".xlsx"));
+                Sheet sheet = workbook.getSheetAt(0);
 
-        try {
-            XSSFWorkbook workbook = (XSSFWorkbook) WorkbookFactory.create(new FileInputStream("sample/data"+sampleSize+".xlsx"));
-            Sheet sheet = workbook.getSheetAt(0);
+                for (Row row : sheet) {
+                    Iterator<Cell> cellIterator = row.cellIterator();
 
-            for (Row row : sheet) {
-                Iterator<Cell> cellIterator = row.cellIterator();
-
-                while (cellIterator.hasNext()) {
-                    Cell cell = cellIterator.next();
-                    if (cell.getColumnIndex() == 0) {
-                        listOfPeople.add(cell.getStringCellValue());
+                    while (cellIterator.hasNext()) {
+                        Cell cell = cellIterator.next();
+                        if (cell.getColumnIndex() == 0) {
+                            listOfPeople.add(cell.getStringCellValue());
+                        }
                     }
                 }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                System.out.println("File not found");
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            System.out.println("First record from sample: " + listOfPeople.get(0));
+            System.out.println("Last record from sample: " + (listOfPeople.get(listOfPeople.size() - 1)));
 
-
-            System.out.println("File not found");
+            for (int idx=0; idx < 3; idx++) {
+                System.out.println("*------------"
+                        + idx + " execution ------------*");
+                synchronousProcessing(listOfPeople);
+                parallelProcessing(listOfPeople);
+                parallelProcessing2(listOfPeople);
+                parallelProcessing3(listOfPeople);
+            }
         }
-
-        System.out.println("First record from sample: " + listOfPeople.get(0));
-        System.out.println("Last record from sample: " + (listOfPeople.get(listOfPeople.size() - 1)));
-
-        synchronousProcessing(listOfPeople);
-        parallelProcessing(listOfPeople);
-        parallelProcessing2(listOfPeople);
-        parallelProcessing3(listOfPeople);
     }
 
     static void synchronousProcessing(List<String> listOfPeople) throws SQLException {
+        System.out.println("\n*--------- Synchronous ----------*");
+
         DbConnection.cleanDatabase();
         long startTime = System.currentTimeMillis();
         listOfPeople.forEach(person -> {
@@ -68,11 +72,16 @@ public class Main {
         long elapsedTime = endTime - startTime;
         System.out.println("[Java] Synchronous implementation took " + elapsedTime + " milliseconds." );
         System.out.println("[Java] Processed " + listOfPeople.size() + " records.");
+        DbConnection.countRows();
+        DbConnection.getFirstRecord();
+        DbConnection.getLastRecord();
     }
 
 
 
     static void parallelProcessing(List<String> listOfPeople) throws SQLException {
+        System.out.println("\n*--------- Parallel 1 ----------*");
+
         DbConnection.cleanDatabase();
         long startTime = System.currentTimeMillis();
         listOfPeople.parallelStream().forEach(person -> {
@@ -86,9 +95,14 @@ public class Main {
         long elapsedTime = endTime - startTime;
         System.out.println("[Java] Parallel implementation 1 took " + elapsedTime + " milliseconds." );
         System.out.println("[Java] Processed " + listOfPeople.size() + " records.");
+        DbConnection.countRows();
+        DbConnection.getFirstRecord();
+        DbConnection.getLastRecord();
     }
 
     static void parallelProcessing2(List<String> listOfPeople) throws SQLException {
+        System.out.println("\n*--------- Parallel 2 ----------*");
+
         DbConnection.cleanDatabase();
         ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
         long startTime = System.currentTimeMillis();
@@ -113,9 +127,14 @@ public class Main {
         long elapsedTime = endTime - startTime;
         System.out.println("[Java] Parallel implementation 2 took " + elapsedTime + " milliseconds." );
         System.out.println("[Java] Processed " + listOfPeople.size() + " records.");
+        DbConnection.countRows();
+        DbConnection.getFirstRecord();
+        DbConnection.getLastRecord();
     }
 
     static void parallelProcessing3(List<String> listOfPeople) throws SQLException {
+        System.out.println("\n*--------- Parallel 3 ----------*");
+
         DbConnection.cleanDatabase();
         long startTime = System.currentTimeMillis();
         ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
@@ -124,5 +143,8 @@ public class Main {
         long elapsedTime = endTime - startTime;
         System.out.println("[Java] Parallel implementation 3 took " + elapsedTime + " milliseconds." );
         System.out.println("[Java] Processed " + listOfPeople.size() + " records.");
+        DbConnection.countRows();
+        DbConnection.getFirstRecord();
+        DbConnection.getLastRecord();
     }
 }
